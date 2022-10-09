89b6ccb29f3d4737b746f6291caf3d092076e274
mdrasel101126 <mdrasel101126@gmail.com>
2022-10-10 00:24:43 +0600

    order review added

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { removeFromDb } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
-import Product from "../Product/Product";
 import ReviewItem from "../ReviewItem/ReviewItem";
 import "./Orders.css";
 const Orders = () => {
-  const { products, previousCart } = useLoaderData();
+  const { previousCart } = useLoaderData();
   const [cart, setCart] = useState(previousCart);
+  const handleRemoveItem = (id) => {
+    const remainingCart = cart.filter((product) => product.id !== id);
+    setCart(remainingCart);
+    removeFromDb(id);
+  };
   return (
     <div className="shop-container">
       <div className="orders-container">
         {cart.map((product) => (
-          <ReviewItem key={product.id} product={product}></ReviewItem>
+          <ReviewItem
+            key={product.id}
+            product={product}
+            handleRemoveItem={handleRemoveItem}
+          ></ReviewItem>
         ))}
       </div>
       <div className="cart-container">
--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,11 +1,37 @@
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import "./ReviewItem.css";
 
-const ReviewItem = ({ product }) => {
-  console.log(product);
-  const { name, quantity, price } = product;
+const ReviewItem = ({ product, handleRemoveItem }) => {
+  const { id, name, quantity, price, img, shipping } = product;
   return (
-    <div>
-      <h1>This is Review Item</h1>
+    <div className="review-item">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div className="review-details-container">
+        <div className="review-details">
+          <p>{name}</p>
+          <p>
+            <small>Price: ${price}</small>
+          </p>
+          <p>
+            <small>Shipping: ${shipping}</small>
+          </p>
+          <p>
+            <small>Quantity: ${quantity}</small>
+          </p>
+        </div>
+        <div className="review-delete-container">
+          <button onClick={() => handleRemoveItem(id)} className="btn-delete">
+            <FontAwesomeIcon
+              className="delete-icon"
+              icon={faTrashAlt}
+            ></FontAwesomeIcon>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
